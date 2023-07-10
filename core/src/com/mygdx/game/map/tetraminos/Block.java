bc387122f8cd551961ff5a69c175dc4207790b87
FedorK <94034748+FedorKolotilin@users.noreply.github.com>
2023-07-10 14:02:21 +0300

    addedMapAndBlock

--- core/src/com/mygdx/game/map/tetraminos/Block.java
+++ core/src/com/mygdx/game/map/tetraminos/Block.java
@@ -1,4 +1,18 @@
 package com.mygdx.game.map.tetraminos;
 
-public class Block {
+import com.badlogic.gdx.graphics.Texture;
+import com.mygdx.game.ui.ImageView;
+
+public class Block extends ImageView {
+    int type;
+
+    public Block(float x, float y, float width, float height, String imgSource, int type) {
+        super(x, y, width, height, imgSource);
+        this.type = type;
+    }
+
+    public Block(float x, float y, float width, float height, Texture imgTexture, int type) {
+        super(x, y, width, height, imgTexture);
+        this.type = type;
+    }
 }
