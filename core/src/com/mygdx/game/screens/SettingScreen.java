33cf0436847c912b1eb37e15b191929dacfedcb2
AndreyStelletskiy <a.stelletskiy@yandex.ru>
2023-07-10 16:47:32 +0300

    Make Settings

---
+++ core/src/com/mygdx/game/screens/SettingScreen.java
@@ -0,0 +1,95 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.ui.ImageView;
+import com.mygdx.game.ui.TextButton;
+import com.mygdx.game.ui.TextView;
+import com.mygdx.game.ui.UiComponent;
+import com.mygdx.game.utils.GameSettings;
+
+import java.util.ArrayList;
+
+public class SettingScreen implements Screen {
+    ArrayList<UiComponent> uiComponentsList;
+    MyGdxGame myGdxGame;
+
+
+    public SettingScreen(MyGdxGame myGdxGame) {
+        this.myGdxGame = myGdxGame;
+        uiComponentsList = new ArrayList<>();
+
+        ImageView background = new ImageView(0, 0, GameSettings.SCR_WIDTH, GameSettings.SCR_HEIGHT, "backgrounds/homeBG.png");
+        TextView title = new TextView(myGdxGame.largeFont.bitmapFont, "Settings", -1, 1825);
+
+        TextButton buttonExit = new TextButton(myGdxGame.largeFont.bitmapFont, "Return home", 25, 175);
+        buttonExit.setOnClickListener(onReturnButtonClickListener);
+        uiComponentsList.add(background);
+        uiComponentsList.add(title);
+        uiComponentsList.add(buttonExit);
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+        if (Gdx.input.justTouched()) {
+            myGdxGame.touch.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            myGdxGame.touch = myGdxGame.camera.unproject(myGdxGame.touch);
+            for (UiComponent component : uiComponentsList) {
+                if(component.isVisible) component.isHit(myGdxGame.touch.x, myGdxGame.touch.y);
+            }
+        }
+
+        ScreenUtils.clear(0, 0, 0, 1);
+        myGdxGame.camera.update();
+        myGdxGame.batch.setProjectionMatrix(myGdxGame.camera.combined);
+        myGdxGame.batch.begin();
+
+        for (UiComponent component : uiComponentsList) {
+            component.draw(myGdxGame);
+        }
+
+        myGdxGame.batch.end();
+    }
+
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+    UiComponent.OnClickListener onReturnButtonClickListener = new UiComponent.OnClickListener() {
+        @Override
+        public void onClicked() {
+            Gdx.app.debug("onClicked", "onReturnButtonClicked");
+            myGdxGame.setScreen(myGdxGame.menuScreen);
+        }
+    };
+}
