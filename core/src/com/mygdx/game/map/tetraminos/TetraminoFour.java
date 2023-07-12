6da6d498f97810c2df9075cc31750175cdd8a7ac
Koraniel <leolewka@gmail.com>
2023-07-12 14:11:23 +0300

    Пофиксил баг с вращением, Сделал матричный поворот

--- core/src/com/mygdx/game/map/tetraminos/TetraminoFour.java
+++ core/src/com/mygdx/game/map/tetraminos/TetraminoFour.java
@@ -5,10 +5,8 @@ import static com.mygdx.game.utils.MathHelper.percent;
 import com.mygdx.game.map.Map;
 
 public class TetraminoFour extends AbstractTetramino{
-    private boolean isVertical;
     public TetraminoFour(int centralX, int centralY) {
         INDEX = 4;
-        isVertical = false;
         coordinatesX = new int[4];
         coordinatesY = new int[4];
         coordinatesX[0] = centralX - 1;
@@ -19,98 +17,5 @@ public class TetraminoFour extends AbstractTetramino{
         coordinatesY[1] = centralY;
         coordinatesY[2] = centralY;
         coordinatesY[3] = centralY;
-    }
-    @Override
-    public void rotateRight(Map map) {
-        int[] bufferX = new int[4];
-        int[] bufferY = new int[4];
-        System.arraycopy(coordinatesX, 0, bufferX, 0, 4);
-        System.arraycopy(coordinatesY, 0, bufferY, 0, 4);
-
-        if (isVertical) {
-            coordinatesX[0] = coordinatesX[1] - 1;
-            coordinatesX[2] = coordinatesX[1] + 1;
-            coordinatesX[3] = coordinatesX[1] + 2;
-            coordinatesY[0] = coordinatesY[1];
-            coordinatesY[2] = coordinatesY[1];
-            coordinatesY[3] = coordinatesY[1];
-            isVertical = false;
-        } else {
-            coordinatesY[0] = coordinatesY[1] - 1;
-            coordinatesY[2] = coordinatesY[1] + 1;
-            coordinatesY[3] = coordinatesY[1] + 2;
-            coordinatesX[0] = coordinatesX[1];
-            coordinatesX[2] = coordinatesX[1];
-            coordinatesX[3] = coordinatesX[1];
-            isVertical = true;
-        }
-        for(int i = 0; i < 4; i ++){
-            coordinatesX[i] = percent(coordinatesX[i], map.width);
-            coordinatesY[i] = percent(coordinatesY[i], map.height);
-        }
-
-        if (!map.dontTetraminoConflict(this)) {
-            this.moveLeft(map);
-            if (!map.dontTetraminoConflict(this)) {
-                this.moveRight(map);
-                this.moveRight(map);
-            if (!map.dontTetraminoConflict(this)) {
-                this.moveLeft(map);
-                this.moveUp(map);
-                if (!map.dontTetraminoConflict(this)) {
-                    System.arraycopy(bufferX, 0, coordinatesX, 0, 4);
-                    System.arraycopy(bufferY, 0, coordinatesY, 0, 4);
-                    this.isMovable = false;
-                }
-            }
-            }
-        }
-    }
-    @Override
-    public void rotateLeft(Map map) {
-        int[] bufferX = new int[4];
-        int[] bufferY = new int[4];
-        System.arraycopy(coordinatesX, 0, bufferX, 0, 4);
-        System.arraycopy(coordinatesY, 0, bufferY, 0, 4);
-
-        if (isVertical) {
-            coordinatesX[0] = coordinatesX[1] - 1;
-            coordinatesX[2] = coordinatesX[1] + 1;
-            coordinatesX[3] = coordinatesX[1] + 2;
-            coordinatesY[0] = coordinatesY[1];
-            coordinatesY[2] = coordinatesY[1];
-            coordinatesY[3] = coordinatesY[1];
-            isVertical = false;
-        } else {
-            coordinatesY[0] = coordinatesY[1] - 1;
-            coordinatesY[2] = coordinatesY[1] + 1;
-            coordinatesY[3] = coordinatesY[1] + 2;
-            coordinatesX[0] = coordinatesX[1];
-            coordinatesX[2] = coordinatesX[1];
-            coordinatesX[3] = coordinatesX[1];
-            isVertical = true;
-        }
-        for(int i = 0; i < 4; i ++){
-            coordinatesX[i] = percent(coordinatesX[i], map.width);
-            coordinatesY[i] = percent(coordinatesY[i], map.height);
-        }
-
-
-        if (!!map.dontTetraminoConflict(this)) {
-            this.moveLeft(map);
-            if (!map.dontTetraminoConflict(this)) {
-                this.moveRight(map);
-                this.moveRight(map);
-            }
-            if (!map.dontTetraminoConflict(this)) {
-                this.moveLeft(map);
-                this.moveUp(map);
-                if (!map.dontTetraminoConflict(this)) {
-                    System.arraycopy(bufferX, 0, coordinatesX, 0, 4);
-                    System.arraycopy(bufferY, 0, coordinatesY, 0, 4);
-                    this.isMovable = false;
-                }
-            }
-        }
     }
 }
--- core/src/com/mygdx/game/map/tetraminos/TetraminoTwo.java
+++ core/src/com/mygdx/game/map/tetraminos/TetraminoTwo.java
@@ -5,8 +5,6 @@ import static com.mygdx.game.utils.MathHelper.percent;
 import com.mygdx.game.map.Map;
 
 public class TetraminoTwo extends AbstractTetramino{
-
-    private boolean isPositionVertical;
     public TetraminoTwo(int centralX, int centralY) { //опорная координата - x[1]y[1]
         INDEX = 2;
         coordinatesX = new int[4];
@@ -19,96 +17,5 @@ public class TetraminoTwo extends AbstractTetramino{
         coordinatesY[1] = centralY;
         coordinatesY[2] = centralY - 1;
         coordinatesY[3] = centralY - 1;
-        isPositionVertical = false;
-    }
-    @Override
-    public void rotateRight(Map map) {
-        int[] bufferX = new int[4];
-        int[] bufferY = new int[4];
-        System.arraycopy(coordinatesX, 0, bufferX, 0, 4);
-        System.arraycopy(coordinatesY, 0, bufferY, 0, 4);
-        if (this.isPositionVertical) {
-            coordinatesX[0] = coordinatesX[1] - 1;
-            coordinatesX[2] = coordinatesX[1];
-            coordinatesX[3] = coordinatesX[1] + 1;
-            coordinatesY[0] = coordinatesY[1];
-            coordinatesY[2] = coordinatesY[1] - 1;
-            coordinatesY[3] = coordinatesY[1] - 1;
-            isPositionVertical = false;
-        } else {
-            coordinatesX[0] = coordinatesX[1];
-            coordinatesX[2] = coordinatesX[1] + 1;
-            coordinatesX[3] = coordinatesX[1] + 1;
-            coordinatesY[0] = coordinatesY[1] - 1;
-            coordinatesY[2] = coordinatesY[1];
-            coordinatesY[3] = coordinatesY[1] + 1;
-            isPositionVertical = true;
-        }
-        for(int i = 0; i < 4; i ++){
-            coordinatesX[i] = percent(coordinatesX[i], map.width);
-            coordinatesY[i] = percent(coordinatesY[i], map.height);
-        }
-        if (!map.dontTetraminoConflict(this)) {
-            this.moveLeft(map);
-            if (!map.dontTetraminoConflict(this)) {
-                this.moveRight(map);
-                this.moveRight(map);
-            }
-            if (!map.dontTetraminoConflict(this)) {
-                this.moveLeft(map);
-                this.moveUp(map);
-                if (!map.dontTetraminoConflict(this)) {
-                    System.arraycopy(bufferX, 0, coordinatesX, 0, 4);
-                    System.arraycopy(bufferY, 0, coordinatesY, 0, 4);
-                    this.isMovable = false;
-                }
-            }
-        }
-    }
-    @Override
-    public void rotateLeft(Map map) {
-        int[] bufferX = new int[4];
-        int[] bufferY = new int[4];
-        System.arraycopy(coordinatesX, 0, bufferX, 0, 4);
-        System.arraycopy(coordinatesY, 0, bufferY, 0, 4);
-        if (this.isPositionVertical) {
-            coordinatesX[0] = coordinatesX[1] - 1;
-            coordinatesX[2] = coordinatesX[1];
-            coordinatesX[3] = coordinatesX[1] + 1;
-            coordinatesY[0] = coordinatesY[1];
-            coordinatesY[2] = coordinatesY[1] - 1;
-            coordinatesY[3] = coordinatesY[1] - 1;
-            isPositionVertical = false;
-        } else {
-            coordinatesX[0] = coordinatesX[1];
-            coordinatesX[2] = coordinatesX[1] + 1;
-            coordinatesX[3] = coordinatesX[1] + 1;
-            coordinatesY[0] = coordinatesY[1] - 1;
-            coordinatesY[2] = coordinatesY[1];
-            coordinatesY[3] = coordinatesY[1] + 1;
-            isPositionVertical = true;
-        }
-
-        
-        for(int i = 0; i < 4; i ++){
-            coordinatesX[i] = percent(coordinatesX[i], map.width);
-            coordinatesY[i] = percent(coordinatesY[i], map.height);
-        }
-        if (!map.dontTetraminoConflict(this)) {
-            this.moveLeft(map);
-            if (!map.dontTetraminoConflict(this)) {
-                this.moveRight(map);
-                this.moveRight(map);
-            }
-            if (!map.dontTetraminoConflict(this)) {
-                this.moveLeft(map);
-                this.moveUp(map);
-                if (!map.dontTetraminoConflict(this)) {
-                    System.arraycopy(bufferX, 0, coordinatesX, 0, 4);
-                    System.arraycopy(bufferY, 0, coordinatesY, 0, 4);
-                    this.isMovable = false;
-                }
-            }
-        }
     }
 }
